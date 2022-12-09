d16f8d9844db7afaab64975d7f7896d1508382d9
fhackett <fhackett.py@gmail.com>
2022-12-09 02:04:35 -0800

    address Ivan's comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,32 @@
 import { defineConfig } from 'astro/config';
 import { readFileSync } from "fs";
-
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import svelte from "@astrojs/svelte";
+import solidJs from "@astrojs/solid-js";
 
 export default defineConfig({
-  integrations: [tailwind(), mdx(), svelte()],
+  integrations: [tailwind(), mdx(), solidJs()],
   markdown: {
     shikiConfig: {
       theme: 'dracula',
       langs: [
-        // important: a language's dependencies must be loaded first, or explosion
-        {
-          id: 'tlaplus',
-          scopeName: 'source.tlaplus',
-          grammar: JSON.parse(readFileSync('./highlighting/tlaplus.tmLanguage.json')),
-          aliases: ['tla'],
-        },
-        {
-          id: 'pluscal',
-          scopeName: 'source.tlaplus.pluscal',
-          grammar: JSON.parse(readFileSync('./highlighting/pluscal.tmLanguage.json')),
-          embeddedLangs: ['tlaplus'],
-        },
-        {
-          id: 'mpcal',
-          scopeName: 'source.tlaplus.mpcal',
-          grammar: JSON.parse(readFileSync('./highlighting/mpcal.tmLanguage.json')),
-          embeddedLangs: ['tlaplus', 'pluscal'],
-        },
-      ],
+      // important: a language's dependencies must be loaded first, or explosion
+      {
+        id: 'tlaplus',
+        scopeName: 'source.tlaplus',
+        grammar: JSON.parse(readFileSync('./highlighting/tlaplus.tmLanguage.json')),
+        aliases: ['tla']
+      }, {
+        id: 'pluscal',
+        scopeName: 'source.tlaplus.pluscal',
+        grammar: JSON.parse(readFileSync('./highlighting/pluscal.tmLanguage.json')),
+        embeddedLangs: ['tlaplus']
+      }, {
+        id: 'mpcal',
+        scopeName: 'source.tlaplus.mpcal',
+        grammar: JSON.parse(readFileSync('./highlighting/mpcal.tmLanguage.json')),
+        embeddedLangs: ['tlaplus', 'pluscal']
+      }],
       wrap: true
     }
   }
---
+++ src/layouts/meta.js
@@ -0,0 +1,4 @@
+export let borderColor = "border-slate-600 dark:border-slate-400"
+export let bgColor = "bg-white dark:bg-black"
+export let borderStyle = "border"
+export let buttonColors = "border-slate-400 dark:border-slate-600 bg-slate-200 dark:bg-slate-800"
